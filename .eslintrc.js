27ca4e9815f39373f061983fdef50a90c67ec120
liuhuan <1341452366@qq.com>
2022-08-02 14:33:37 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('eslint-define-config');
+module.exports = defineConfig({
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  },
+});
