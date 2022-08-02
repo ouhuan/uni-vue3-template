dfe276d7ad0c1df3ad9555dd2cf217437f76a92b
liuhuan <1341452366@qq.com>
2022-08-02 15:17:39 +0800

    ci: config

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,7 +5,7 @@
       src="/static/logo.png"
     />
     <view class="text-area">
-      <div />
+      <div></div>
       <text class="title">
         {{ title }}
       </text>
@@ -14,8 +14,12 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from 'vue';
+const title = ref('Hello');
+const props = defineProps<{
+  name: string
+}>();
+console.log('props', props);
 </script>
 
 <style lang="scss">
@@ -24,15 +28,6 @@ const title = ref('Hello')
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
 }
 
 .text-area {
