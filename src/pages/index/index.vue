d2327a8d3a42c7b0596424b543c5d27bc45efeea
liuhuan <1341452366@qq.com>
2022-08-05 10:38:24 +0800

    ci: 修改模板文件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,22 +1,9 @@
 <template>
-  <view class="content">
-    <DataItem>
-      <template v-for="item in ['1','2']" :key="item" v-slot:[item]>
-        <div>{{ item }}</div>
-      </template>
-    </DataItem>
-  </view>
+  <view class="content"/>
 </template>
 
 <script setup lang="ts">
-import DataItem from './DataItem.vue';
 </script>
 
 <style lang="scss">
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
 </style>
